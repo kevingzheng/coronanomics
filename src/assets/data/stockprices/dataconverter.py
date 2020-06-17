a368177aac907c5fd0caae1323a7103779ae2876
hegdep <hegdep@umich.edu>
2020-06-16 21:05:29 -0400

    Added DJIA data and reconfigured python data conversion script

--- src/assets/data/stockprices/dataconverter.py
+++ src/assets/data/stockprices/dataconverter.py
@@ -7,13 +7,15 @@ import sys
 #    $ python ./dataconverter.py [dashes | slashes] input_file.csv
 # The input file must be in a format similar to that of SPY_1yr.csv, with month-day-year
 def main():
-    if sys.argv[1] == "dashes":
+    if sys.argv[1] == "normal":
+        dashesalt()
+    elif sys.argv[1] == "dashes":
         dashes("-")
     elif sys.argv[1] == "slashes":
         dashes("/")
     else:
         print("Error: script mode not recognized. Please use the script as follows: \n")
-        print("python dataconverter.py [dashes | slashes] input_file.csv")
+        print("python dataconverter.py [dashes | slashes | normal] input_file.csv")
 
 def dashes(inputchar):
     of = open(sys.argv[2][:-4] + '_new.csv', "a")
@@ -36,6 +38,29 @@ def dashes(inputchar):
                     outfile.write(result + ',' + rows[1][1:] + '\n')
                 else:
                     counter = 1
+
+def dashesalt():
+    of = open(sys.argv[2][:-4] + '_new.csv', "a")
+    of.write("date,value\n")
+
+    with open(sys.argv[2], mode='r') as infile:
+        reader = csv.reader(infile)
+
+        with of as outfile:
+            # writer = csv.writer(outfile)
+            counter = 0
+            for rows in reader:
+                if counter != 0:
+                    result = ""
+                    result += rows[0][:4]
+                    result += "-"
+                    result += rows[0][5:7]
+                    result += "-"
+                    result += rows[0][8:10]
+                    outfile.write(result + ',' + rows[1][1:] + '\n')
+                else:
+                    counter = 1
+
 
 if __name__ == "__main__":
     main()
